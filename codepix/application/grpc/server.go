a3249b605e2b8ed068a0b6790ddb1509af633ffb
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2021-02-07 14:30:34 -0300

    proto-factory to integrate the pix use cases with the gprc server

--- codepix/application/grpc/server.go
+++ codepix/application/grpc/server.go
@@ -5,17 +5,27 @@ import (
 	"log"
 	"net"
 
+	"github.com/LuizEduardoCardozo/imercao-fullcycle/codepix-go/application/grpc/pb"
+	"github.com/LuizEduardoCardozo/imercao-fullcycle/codepix-go/application/usecase"
+	"github.com/LuizEduardoCardozo/imercao-fullcycle/codepix-go/infra/repository"
 	"github.com/jinzhu/gorm"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 // StartGrpcServer starts the grpc server into a specific port
 func StartGrpcServer(database *gorm.DB, port int) {
 
 	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+
+	pixRepository := repository.PixKeyRepositoryDB{DB: database}
+	pixUseCase := usecase.PixUseCase{PixKeyRepository: pixRepository}
+	pixGrpcService := NewPixGrpcService(pixUseCase)
+
+	pb.RegisterPixServiceServer(grpcServer, pixGrpcService)
 
 	address := fmt.Sprintf("0.0.0.0:%d", port)
-
 	listener, err := net.Listen("tcp", address)
 
 	if err != nil {
